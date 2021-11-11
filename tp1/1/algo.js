f382fed001cffbaba5a287a96d4c0a2f0d8594f2
gigip17 <70454351+gigip17@users.noreply.github.com>
2021-11-11 02:57:27 +0100

    Update algo.js

--- tp1/1/algo.js
+++ tp1/1/algo.js
@@ -1,5 +1,23 @@
 exports = module.exports = function (input) {
-    // YOUR CODE BETWEEN HERE
-
-    // AND HERE
-}
+  // YOUR CODE BETWEEN HERE
+  var classement = input[0];
+  var i = 1;
+  for (; i < input.length; i++) {
+    var majClass = input[i].split(" ");
+    // je divise la chaine de caractère à l'espace
+    classement += parseInt(majClass[0]);
+    //je met à jour mon classement avec la variable majClass
+    // qui convertit la premiere string avant l'espace en entier
+    classement -= parseInt(majClass[1]);
+    // j'utilise les opérateurs += et -= changer mon classement
+  }
+  //   je retourne l'argent gagner si il y a, ou KO
+  if (classement <= 100) {
+    return 1000;
+  } else if (classement > 100 && classement <= 10000) {
+    return 100;
+  } else if (classement > 10000) {
+    return "KO";
+  }
+  // AND HERE
+};
