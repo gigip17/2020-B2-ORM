a97368456fee0c59b77dbbd7d2ff5278052c7041
gigip17 <70454351+gigip17@users.noreply.github.com>
2021-11-11 02:58:33 +0100

    Update algo.js

--- tp1/2/algo.js
+++ tp1/2/algo.js
@@ -1,5 +1,22 @@
 exports = module.exports = function (input) {
-    // YOUR CODE BETWEEN HERE
+  // YOUR CODE BETWEEN HERE
+  //   var nombreDeCarton = input[0];
+  var chargeTotal = 0;
+  var i = 1;
+  var allerRetour = 0;
+  for (; i < input.length; i++) {
+    var poidsCarton = input[i];
+    chargeTotal += poidsCarton;
+  }
+  if (chargeTotal <= 100) {
+    return ++allerRetour;
+  } else if ((chargeTotal >= 100) | (chargeTotal <= 200)) {
+    return (allerRetour = 2);
+  }
+  // j'ai pas reussi a trouver comment +1 a tous les 100
 
-    // AND HERE
-}
+  //   for (allerRetour; chargeTotal < 100; allerRetour++) {
+  //     console.log(allerRetour);
+
+  // AND HERE
+};
